3902ec4974fab9b6f1c116998ccbe27ab25e754a
Aishani Bhalla <aishani.bhalla0709@gmail.com>
2017-04-03 15:48:55 -0400

    test contact form

--- js/contact_me.js
+++ js/contact_me.js
@@ -12,7 +12,7 @@ $(function() {
             // get values from FORM
             var name = $("input#name").val();
             var email = $("input#email").val();
-            var phone = $("input#phone").val();
+            // var phone = $("input#phone").val();
             var message = $("textarea#message").val();
             var firstName = name; // For Success/Failure Message
             // Check for white space in name for Success/Fail message
@@ -20,11 +20,11 @@ $(function() {
                 firstName = name.split(' ').slice(0, -1).join(' ');
             }
             $.ajax({
-                url: "././mail/contact_me.php",
+                url: "../mail/contact_me.php",
                 type: "POST",
                 data: {
                     name: name,
-                    phone: phone,
+                    // phone: phone,
                     email: email,
                     message: message
                 },
